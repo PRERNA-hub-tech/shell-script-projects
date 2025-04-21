df09b739f2246695d39c2c782a4ed0296b797c4b
Prerna <choureynikki2@gmail.com>
2025-04-22 00:41:28 +0530

    new trial

--- script1/file_analyzer.sh
+++ script1/file_analyzer.sh
@@ -13,19 +13,37 @@ read filename #takes the input in variable filename
 
 #check if the file exists
 
-if [ -e "$filename" ]; then
-	echo "File found"
-	lines=$(wc -l<"$filename")
-	words=$(wc -w<"$filename")
-	chars=$(wc -m<"$filename")
 
-	echo "Lines: $lines"
-	echo "Words: $words"
-	echo "Charecters: $chars"
-	
+if [[-e "$filename"]]; then
+	filepath="$filename"
+
 else
-	echo "File not Found!"
+	echo "File not found in current directory, searching in the system..."
+	filepath=$(find / type -f -name "$filename" 2>/dev/null | head -n 1)
 
+	if[[-z "$filepath"]]; then
+		echo "File not found in the system."
+		exit 1
+	else
+		echo "File found at : $filepath"
+	fi
 fi
+
+
+
+# if [ -e "$filename" ]; then
+# 	echo "File found"
+# 	lines=$(wc -l<"$filename")
+# 	words=$(wc -w<"$filename")
+# 	chars=$(wc -m<"$filename")
+
+# 	echo "Lines: $lines"
+# 	echo "Words: $words"
+# 	echo "Charecters: $chars"
+	
+# else
+# 	echo "File not Found!"
+
+# fi
 
 
